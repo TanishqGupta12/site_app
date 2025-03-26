4cb692ed12a30d314c376028b7e751a971318b5c
Tanishq Gupta <tanishq@hipster-dev.com>
2025-03-26 18:26:01 +0530

    adding

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @event =  Event.all.first
-    @categories_data = Category.includes(:captions).where(event_id: @event.try(:id))
+    @categories_data = Category.includes(:sub_captions).where(event_id: @event.try(:id))
     @teacher_data =  User.teachers_for_event(@event.id)
     @course_data =  Course.courses_for_event(@event.id)
   end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_many :sub_captions
+
+  belongs_to :event
 end
