4f58001ce188b468fc512db14dfeb8a41d5b2970
Tanishq Gupta <tanishq@hipster-dev.com>
2025-03-26 16:16:30 +0530

    footer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
 
   def index
     @event =  Event.all.first
+    @categories_data = Category.includes(:captions).where(event_id: @event.try(:id))
   end
 end
