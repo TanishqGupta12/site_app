1a9abe42edf70a2247ac4c9394a77a00b5a2df07
Tanishq Gupta <tanishq@hipster-dev.com>
2025-03-26 07:25:47 +0530

    model

---
+++ app/models/form_section_field.rb
@@ -0,0 +1,2 @@
+class FormSectionField < ApplicationRecord
+end
--- db/migrate/20250325233851_create_email_contents.rb
+++ db/migrate/20250325233851_create_email_contents.rb
@@ -1,4 +1,4 @@
-class CreateCreateEmailContents < ActiveRecord::Migration[7.1]
+class CreateEmailContents < ActiveRecord::Migration[7.1]
   def change
     create_table :email_contents do |t|
 
