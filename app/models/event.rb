cd4c522414fd81d01ab49877db048a69cb26bc0c
Tanishq Gupta <tanishq@hipster-dev.com>
2025-03-26 22:19:25 +0530

    file

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
   has_many :categories
+  has_many_attached :gallery
+
+  attribute :remove_gallery, :boolean
+  after_save -> { gallery.purge }, if: :remove_gallery
 end
